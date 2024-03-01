88db68a04f70b6aaf2e14f077bdc03646ca750a0
Daniel Alvarez <39070588+danielalvarezm527@users.noreply.github.com>
2024-03-01 18:54:32 -0500

    Update Persona.py

--- IntroduccionPOO/Persona.py
+++ IntroduccionPOO/Persona.py
@@ -4,16 +4,39 @@ class Persona:
         self.edad = edad
         self.dni = dni
 
+    def set_attr(self, nombre="", edad="", dni=""):
+        if nombre != "":
+            self.nombre = nombre
+
+        if edad != "":
+            self.edad = edad
+
+        if dni != "":
+            self.dni = dni
+
+    def get_m_attr(self, nombre=False, edad=False, dni=False):
+        if nombre == True:
+            return self.nombre
+
+        if edad == True:
+            return self.edad
+
+        if dni == True:
+            return self.dni
+
+        return ""
+
     def mostrar(self):
-        print(f"Mi nombre es {self.nombre}, tengo {self.edad} años y mi dni es {self.dni}")
+       print(f"Mi nombre es {self.nombre}, tengo {self.edad} años y mi dni es {self.dni}")
 
     def mayorDeEdad(self):
-        if self.edad >= 18:
-            print(f"{self.nombre} es mayor de edad")
-        else:
-            print(f"{self.nombre} no es mayor de edad")
+       if self.edad >= 18:
+           print(f"{self.nombre} es mayor de edad {True}")
+       else:
+           print(f"{self.nombre} no es mayor de edad {False}")
             
 daniel = Persona("Daniel", 26, "1037656417")
-
 daniel.mostrar()
 daniel.mayorDeEdad()
+
+print(f"Utilizando getter para el nombre: {daniel.get_m_attr(True, False, False)}")
