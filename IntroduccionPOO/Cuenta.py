df135ba06f2fa07fb7f16b8e687d18095aa06c45
Daniel Alvarez <39070588+danielalvarezm527@users.noreply.github.com>
2024-03-01 19:11:33 -0500

    Update Cuenta.py

--- IntroduccionPOO/Cuenta.py
+++ IntroduccionPOO/Cuenta.py
@@ -1,16 +1,25 @@
 class Cuenta:
     def __init__(self, titular:str, cantidad:float=0):
-        self.titular = titular
-        self.cantidad = cantidad
+        self.__titular = titular
+        self.__cantidad = cantidad
 
     def mostrar(self):
-        print(f"El titular de la cuenta es {self.titular} y tiene {self.cantidad}")
+        print(f"El titular de la cuenta es {self.__titular} y tiene {self.__cantidad}")
+
+    def set_cantidad_attr(self, valor:float):
+        self.__cantidad = valor
+
+    def get_cantidad_attr(self):
+        return self.__cantidad
 
     def ingresar(self, ingreso:float):
-        self.cantidad += ingreso
+        cantidad = self.__cantidad + ingreso
+        self.set_cantidad_attr(cantidad)
 
     def retirar(self, egreso:float):
-        self.cantidad -= egreso
+        if egreso > 0:
+            cantidad = self.__cantidad - egreso
+            self.set_cantidad_attr(cantidad)
 
 mi_cuenta = Cuenta("Daniel Alvarez",1000)
 
@@ -19,3 +28,5 @@ mi_cuenta.ingresar(1000)
 mi_cuenta.mostrar()
 mi_cuenta.retirar(500.7)
 mi_cuenta.mostrar()
+
+print(f"Cantidad utilizando getters {mi_cuenta.get_cantidad_attr()}")
